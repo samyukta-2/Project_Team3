093a83b0f9d1a6cb831386cfa733a75c07af7e0d
samyukta-2 <samyuktar48@gmail.com>
2025-05-26 22:02:23 +0530

    i have added few more lines

--- image.py
+++ image.py
@@ -8,3 +8,4 @@ import matplotlib.pyplot as plt
 from skimage.feature import local_binary_pattern
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
