c8b50ce0491bb2343726ff9320976abe5328085b
samyukta-2 <samyuktar48@gmail.com>
2025-05-26 22:17:21 +0530

    i have added few more lines

--- image.py
+++ image.py
@@ -31,3 +31,5 @@ def extract_lbp_features(image, radius=3, n_points=24):
     hist, _ = np.histogram(lbp.ravel(), bins=np.arange(0, n_points + 3), range=(0, n_points + 2))
     hist = hist.astype('float')
     hist /= (hist.sum() + 1e-7)
+        return hist
+
